001fcbf97436167c8c46b97b44e2f65ee6586e35
Schwo0ps <Schwo0ps@users.noreply.github.com>
2015-10-11 17:34:09 -0700

    removed simplehtmldom

--- simplehtmldom/search.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('simple_html_dom.php');
-
-$html = file_get_html('https://www.crowdfunder.com/user/131000');
-
-foreach($html->find(a.social-meta))
-   echo 'Facebook: '$e->href . '<br>';
-?>
